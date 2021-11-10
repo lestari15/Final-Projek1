76fc2b37a35da63e5dabaec23372f93d220a269d
lestari15 <lestari.18411045@student.ubl.ac.id>
2021-11-10 02:00:26 -0800

    update koding lestari

--- models/reflectionModel.js
+++ models/reflectionModel.js
@@ -1,18 +1,11 @@
 const db = require('../config/connection')
 
-exports.findOne = async (field, value) =>
-  new Promise(async (resolve, reject) => {
-    await db.query(`SELECT * FROM "users" WHERE ${field} = '${value}'`)
-      .then(res => {
-        resolve(res.rows[0])
-      }).catch((err) => {
-        reject(err)
-      })
-  })
-
 exports.insertData = async (value) =>
   new Promise(async (resolve, reject) => {
-    await db.query(`INSERT INTO "users" (nama, email, password, created_at, updated_at) VALUES ('${value.nama}', '${value.email}', '${value.password}', '${value.created_at}', '${value.updated_at}')`)
+    await db.query(`INSERT INTO "reflection" 
+                    (success, low_point, take_away, owner_id, created_at, updated_at) 
+                    VALUES 
+                    ('${value.success}', '${value.low_point}', '${value.take_away}', '${value.owner_id}', '${value.created_at}', '${value.updated_at}')`)
       .then(res => {
         resolve(res)
       }).catch((err) => {
