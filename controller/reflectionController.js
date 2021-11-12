b557517db0dc9998227ecd46d25d17c689d23aa6
rizkyardiansah <muhammadrizkyardiansah93@gmail.com>
2021-11-12 21:10:28 +0700

    add endpoint for delete, update and read reflection

--- controller/reflectionController.js
+++ controller/reflectionController.js
@@ -1,9 +1,15 @@
 const model = require('../models/reflectionModel')
 const response = require('../utils/response')
+const {validateData} = require('../utils/validateData')
 const moment = require('moment')
 
 exports.createDataReflection = (data, ownerId) =>
   new Promise(async (resolve, reject) => {
+    //validate reflection
+    if (validateData(data) != true) {
+      reject(response.commonDataError(validateData(data)))
+    }
+
     Object.assign(data, {
       owner_id: ownerId,
       created_at: moment().format(),
@@ -17,3 +23,86 @@ exports.createDataReflection = (data, ownerId) =>
         reject(response.commonError)
       })
   })
+
+exports.getUserReflections = (ownerId) => {
+  return new Promise((resolve, reject) => {
+    model.getDataByOwnerId(ownerId)
+      .then(res => {
+        resolve(response.commonSuccessWithData(res))
+      })
+      .catch(err => {
+        console.log(err)
+        reject(response.commonError)
+      })
+  })
+}
+
+exports.deleteReflectionById = (reflectionId, ownerId) => {
+  return new Promise((resolve, reject) => {
+    model.getDataByDataId(reflectionId)
+      .then(data => {
+        //check if reflection with x id is exist or not
+        if (data.rowCount < 1) {
+          reject(response.commonNotFoundMessage("Reflection not found"))
+        } 
+      
+        //check if reflection with x id is owned by user y
+        if (data.rows[0].owner_id != ownerId) {
+          reject(response.commonErrormessage("Not allowed to delete this reflection"))
+        } 
+
+        //delete the reflection
+        model.deleteData(reflectionId, ownerId)
+          .then(res => {
+            resolve(response.commonSuccess)
+          })
+          .catch(err => {
+            reject(response.commonError)
+          })  
+      })
+      .catch(err => {
+        reject(response.commonError)
+      })
+  })
+}
+
+exports.updateReflectionById = (reflectionId, ownerId, value) => {
+  return new Promise((resolve, reject) => {
+    model.getDataByDataId(reflectionId)
+      .then(data => {
+        //check if reflection with x id is exist or not
+        if (data.rowCount < 1) {
+          reject(response.commonNotFoundMessage('Reflection not found'))
+        }
+        
+        //check if reflection with x id is owned by user y
+        if (data.rows[0].owner_id != ownerId) {
+          reject(response.commonErrormessage('Not allowed to edit this reflection'))
+        }
+
+        //validate reflection
+        if (validateData(value) != true) {
+          reject(response.commonDataError(validateData(value)))
+        }
+
+        //add property "updated_at" to object "value"
+        Object.assign(value, {
+          updated_at: moment().format()
+        })
+
+
+        //update data on database
+        model.updateData(reflectionId, ownerId, value)
+          .then(res => {
+            resolve(response.commonSuccess)
+          })
+          .catch(err => {
+            console.log(err)
+            reject(response.commonError)
+          })
+      })
+      .catch(err => {
+        reject(response.commonError)
+      })
+  })
+}
--- models/reflectionModel.js
+++ models/reflectionModel.js
@@ -12,3 +12,53 @@ exports.insertData = async (value) =>
         reject(err)
       })
   })
+
+exports.getDataByOwnerId = (ownerId) => {
+  return new Promise((resolve, reject) => {
+    db.query(`SELECT * FROM "reflection" WHERE owner_id = '${ownerId}'`)
+      .then(res => {
+        resolve(res.rows)
+      }).catch(err => {
+        reject(err)
+      })
+  })
+}
+
+exports.getDataByDataId = (reflectionId) => {
+  return new Promise((resolve, reject) => {
+    db.query(`SELECT * FROM "reflection" WHERE id = '${reflectionId}'`)
+      .then(res => {
+        resolve(res)
+      })
+      .catch(err => {
+        reject(err)
+      })
+  })
+}
+
+exports.deleteData = (reflectionId, ownerId) => {
+  return new Promise((resolve, reject) => {
+    db.query(`DELETE FROM "reflection" WHERE id = '${reflectionId}' and owner_id = '${ownerId}'`)
+      .then(res => {
+        resolve(res)
+      })
+      .catch(err => {
+        reject(err)
+      })
+  })
+} 
+
+exports.updateData = (reflectionId, ownerId, value) => {
+  return new Promise((resolve, reject) => {
+    db.query(`UPDATE "reflection"
+              set "success" = '${value.success}', "low_point" = '${value.low_point}', 
+              "take_away" = '${value.take_away}', "updated_at" = '${value.updated_at}'
+              WHERE "id" = '${reflectionId}' and owner_id = '${ownerId}'`)
+      .then(res => {
+        resolve(res)
+      })
+      .catch(err => {
+        reject(err)
+      })
+  })
+}
